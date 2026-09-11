0b676a8f133a80864d39defbf1c1805480d2a4c9
groeluke <groeluke@isu.edu>
2026-09-11 13:52:23 -0600

    Give the first commit and gave it the main.X

---
+++ LAB_4_Part_1_Muzak.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=LAB_4_Part_1_Muzak.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/LAB_4_Part_1_Muzak.X.production.hex
